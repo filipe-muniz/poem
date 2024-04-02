e34433e5c0f3eb3e7759a7fbb6aff237a26363db
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2024-04-02 09:38:07 -0300

    feat: nova rota em que dá pra escolher a quantidade de poemas enviados

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Internal Server Error");
-});
+// Isso aqui era importante?
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Internal Server Error');
+// });
 
-const poemsRouter = require("../../routes/poems.js");
+const poemsRouter = require('../routes/poems.js');
 app.use(express.json());
 
 app.listen(PORT, () =>
-  console.log(`App listening on http://localhost:${PORT}/api/poems`)
+  console.log(`App listening on http://localhost:${PORT}/api/poems`),
 );
 
-app.use("/api", poemsRouter);
+app.use('/api', poemsRouter);
