ca9a728219b4b2834f0b4a18a0aab8decb550189
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2024-04-12 01:39:36 -0300

    feat: instalação do cors no server

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// Isso aqui era importante?
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('Internal Server Error');
-// });
-
 const poemsRouter = require('../routes/poems.js');
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () =>
   console.log(`App listening on http://localhost:${PORT}/api/poems`),
